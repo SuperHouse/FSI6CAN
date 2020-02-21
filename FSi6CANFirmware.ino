55077d7a05c096a6c506ca87916738bd8794c8ea
Jonathan Oxer <jon@oxer.com.au>
2020-02-21 14:35:40 +1100

    Added support for changing throttle using keystrokes

--- FSi6CANFirmware.ino
+++ FSi6CANFirmware.ino
@@ -3,6 +3,10 @@
  * 
  * Receives messages via CAN bus and generates analog outputs.
  * 
+ * In the Arduino IDE, select board profile:
+ *  -> Arduino Pro or Pro Mini
+ *    -> ATmega328P (3.3V, 8MHz)
+ * 
  * By Chris Fryer (chris.fryer78@gmail.com) and Jonathan Oxer (jon@superhouse.tv)
  * www.superhouse.tv
  */
@@ -13,10 +17,17 @@
 #include "config.h"
 
 // Settings for analog output
+/*
 const int outputX1 = 9;  // Red
 const int outputY1 = 6;  // Orange
 const int outputX2 = 5;  // Black
 const int outputY2 = 3;  // Brown
+*/
+
+const int outputX1 = 3;  // Red      Rudder
+const int outputY1 = 6;  // Orange   Elevator
+const int outputX2 = 9;  // Black
+const int outputY2 = 5;  // Brown    Throttle
 
 /*
 outputX2 = 9;  // Red
@@ -28,20 +39,22 @@ outputY1 = 3;  // Brown
 int xAxis1Output = 0;
 int yAxis1Output = 0;
 int xAxis2Output = 0;
-int yAxis2Output = 0;
+int yAxis2Output = 255;  // Maximum output is stick full down (throttle off)
 
 long last_report_time = 0;
 unsigned int serial_report_interval = 500;
+
+int throttle_position = 255;  // Maximum output is stick full down (throttle off)
+char incoming_message_buffer[12];  // Buffer for incoming CAN messages
 
 /*
  * 
  */
 void setup() {
-  Serial.begin(19200);
-
+  Serial.begin(9600);
   Serial.println("FSi6 CAN bus interface v2.0");
-  
   Serial.println("Starting CAN Receiver");
+  
   // start the CAN bus at 500 kbps
   if (!CAN.begin(500E3)) {
     Serial.println("Starting CAN failed!");
@@ -71,28 +84,35 @@ void setup() {
  * Loop
  */
 void loop() {
+  yAxis2Output = throttle_position;
+
   analogWrite(outputX1, xAxis1Output);
   analogWrite(outputY1, yAxis1Output);
-  analogWrite(outputX2, xAxis2Output);
+  //analogWrite(outputX2, xAxis2Output);
   analogWrite(outputY2, yAxis2Output);
   
   if(OUTPUT_DEBUG)
   {
-  long current_time = millis();
-  if(current_time > (last_report_time + serial_report_interval))
-  {
-    //Serial.print(xAxis1ReportValue);
-    //Serial.print(" | ");
-    Serial.print(xAxis1Output);
-    Serial.print(" | ");
-    Serial.print(yAxis1Output);
-    Serial.print(" | ");
-    Serial.print(xAxis2Output);
-    Serial.print(" | ");
-    Serial.print(yAxis2Output);
-    Serial.println(" | ");
-    last_report_time = current_time;
-  }
+    long current_time = millis();
+    if(current_time > (last_report_time + serial_report_interval))
+    {
+      //Serial.print(xAxis1ReportValue);
+      //Serial.print(" | ");
+      Serial.print("X1:");
+      Serial.print(xAxis1Output);
+      
+      Serial.print(" |Y1:");
+      Serial.print(yAxis1Output);
+      
+      Serial.print(" |X2:");
+      Serial.print(xAxis2Output);
+      
+      Serial.print(" |Y2:");
+      Serial.print(yAxis2Output);
+      
+      Serial.println(" |");
+      last_report_time = current_time;
+    }
   }
 }
 
@@ -105,9 +125,21 @@ void onReceive(int packetSize) {
     int integerValue = 0;    // throw away previous integerValue
     char incomingByte;
     byte is_negative = 0;
+    int i = 0;
+    if(CAN_DEBUG)
+    {
+      Serial.print("ID: 0x");
+      Serial.print(CAN.packetId(), HEX);
+      Serial.print(" Value: ");
+    }
     while (CAN.available()) {
-      //Serial.print((char)CAN.read());
       incomingByte = (char)CAN.read();
+      if(CAN_DEBUG)
+      {
+        Serial.print(incomingByte - 48);
+      }
+      incoming_message_buffer[i] = incomingByte;
+      i++;
       if(incomingByte == '-')
       {
         is_negative = true;
@@ -117,6 +149,11 @@ void onReceive(int packetSize) {
         integerValue = ((incomingByte - 48) + integerValue);
       }
     }
+    if(CAN_DEBUG)
+    {
+      Serial.println("");
+    }
+
   
     if(is_negative)
     {
@@ -132,9 +169,15 @@ void onReceive(int packetSize) {
         Serial.print("X=");
         Serial.print(integerValue, DEC);
       }
-      axis_output = map(integerValue, -75, +75, MIN_OUTPUT_VALUE, MAX_OUTPUT_VALUE);
+      if(INVERT_X1)
+      {
+        axis_output = map(integerValue, +75, -75, MIN_OUTPUT_VALUE, MAX_OUTPUT_VALUE);
+      } else {
+        axis_output = map(integerValue, -75, +75, MIN_OUTPUT_VALUE, MAX_OUTPUT_VALUE);
+      }
       xAxis1Output = constrain(axis_output, MIN_OUTPUT_VALUE, MAX_OUTPUT_VALUE);
     }
+    
     if(CAN.packetId() == 0x13)  // Packet ID 0x13 for Y axis
     {
       if(CAN_DEBUG)
@@ -143,63 +186,57 @@ void onReceive(int packetSize) {
         Serial.println(integerValue, DEC);
       }
       //yAxis1Output = integerValue;
-      axis_output = map(integerValue, -75, +75, MIN_OUTPUT_VALUE, MAX_OUTPUT_VALUE);
+      if(INVERT_Y1)
+      {
+        axis_output = map(integerValue, +75, -75, MIN_OUTPUT_VALUE, MAX_OUTPUT_VALUE);
+      } else {
+        axis_output = map(integerValue, -75, +75, MIN_OUTPUT_VALUE, MAX_OUTPUT_VALUE);
+      }
       yAxis1Output = constrain(axis_output, MIN_OUTPUT_VALUE, MAX_OUTPUT_VALUE);
     }
-}
-
-
-void onReceiveOriginal(int packetSize) {
-  // received a packet
-
-  /*
-  if (CAN.packetRtr()) {
-    Serial.print(" and requested length ");
-    Serial.println(CAN.packetDlc());
-  } else {
-    Serial.print(" and length ");
-    Serial.println(packetSize);
-  */
-  int integerValue = 0;         // throw away previous integerValue
-  char incomingByte;
-  byte is_negative = 0;
-  while (CAN.available()) {
-    //Serial.print((char)CAN.read());
-    incomingByte = (char)CAN.read();
-    if(incomingByte == '-')
-    {
-      is_negative = true;
-    } else {
-      integerValue *= 10;  // shift left 1 decimal place
-      // convert ASCII to integer, add, and shift left 1 decimal place
-      integerValue = ((incomingByte - 48) + integerValue);
-    }
-  }
-  if(is_negative)
-  {
-    integerValue = integerValue * -1;
-  }
-
-  int axis_output = 0;
-  // only print packet data for non-RTR packets
-  if(CAN.packetId() == 0x12)  // Packet ID 0x12 for X axis
-  {
-    //Serial.print("X=");
-    //Serial.print(integerValue, DEC);
-    axis_output = map(integerValue, -110, +110, MIN_OUTPUT_VALUE, MAX_OUTPUT_VALUE);
-    xAxis1Output = constrain(axis_output, MIN_OUTPUT_VALUE, MAX_OUTPUT_VALUE);
-  }
-  if(CAN.packetId() == 0x13)  // Packet ID 0x13 for Y axis
-  {
-    //Serial.print("     Y=");
-    //Serial.println(integerValue, DEC);
-    //yAxis1Output = integerValue;
-    axis_output = map(integerValue, -110, +110, MIN_OUTPUT_VALUE, MAX_OUTPUT_VALUE);
-    yAxis1Output = constrain(axis_output, MIN_OUTPUT_VALUE, MAX_OUTPUT_VALUE);
-  }
     
-  //  while (CAN.available()) {
-  //    Serial.print((char)CAN.read());
-  //  }
-  //Serial.println();
+    if(CAN.packetId() == 0x14)  // Packet ID 0x14 for buttons
+    {
+      if(CAN_DEBUG)
+      {
+        Serial.print("CAN key: ");
+        Serial.println(incoming_message_buffer[0]);
+      }
+      // Check for specific keys to use for throttle up and down
+      if(incoming_message_buffer[0] == 'w' && incoming_message_buffer[1] == '+')  // Throttle up
+      {
+        if(INVERT_Y2)
+        {
+          throttle_position = throttle_position - THROTTLE_INCREMENT;
+        } else {
+          throttle_position = throttle_position + THROTTLE_INCREMENT;
+        }
+      }
+
+      if(incoming_message_buffer[0] == 'v' && incoming_message_buffer[1] == '+')  // Throttle down
+      {
+        if(INVERT_Y2)
+        {
+          throttle_position = throttle_position + THROTTLE_INCREMENT;
+        } else {
+          throttle_position = throttle_position - THROTTLE_INCREMENT;
+        }
+      }
+
+      // Enforce limits
+      if(throttle_position > 255)
+      {
+        throttle_position = 255;
+      }
+      if(throttle_position < 0)
+      {
+        throttle_position = 0;
+      }
+
+      if(OUTPUT_DEBUG)
+      {
+        Serial.print("New throttle position: ");
+        Serial.println(throttle_position);
+      }
+    }
 }
--- config.h
+++ config.h
@@ -1,10 +1,18 @@
 // Debug options
 #define CAN_DEBUG false
-#define OUTPUT_DEBUG true
+#define OUTPUT_DEBUG false
+
+#define THROTTLE_INCREMENT 10
+//#define THROTTLE_STALL_POINT 135   // Values above this, the motor won't turn properly
 
 // Don't output more than this using PWM. Scale to make this 100% output
 #define MAX_OUTPUT_VALUE 255
 #define MID_OUTPUT_VALUE 127
 #define MIN_OUTPUT_VALUE  0
+
+#define INVERT_X1 true
+#define INVERT_Y1 true
+#define INVERT_X2 false
+#define INVERT_Y2 true
 
 #define REFERENCE_OFFSET_MULTIPLIER 0.013 // Magic number!
